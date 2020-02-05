c4150d6ccf793287b8b3e2fc7d45e42426b5a356
Angiecastano1634 <1634@holbertonschool.com>
2020-02-05 22:58:42 +0000

    using printf, fixing codes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - printf
  *
  * Return: Succes(0) Error(1)
  */
-
 int main(void)
 {
-	char *star = "with proper grammar, but the outcome is a piece of art,"
-
-	printf("%s\n", star);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
