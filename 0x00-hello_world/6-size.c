1a7fce7af196c185838fa6076759ab9b178e2530
Angiecastano1634 <1634@holbertonschool.com>
2020-02-05 23:23:28 +0000

    D.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Result: Succes(0) - Error(1)
+ */
+int  main(void)
+{
+	int i;
+	char c;
+	printf("Size of a char: %d", sizeof(c));
+	return (0);
+}
