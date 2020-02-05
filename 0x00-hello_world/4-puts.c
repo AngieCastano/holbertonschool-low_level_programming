362fe459975eaaeb4d3da375cee39d01e3044582
Angiecastano1634 <1634@holbertonschool.com>
2020-02-05 22:23:46 +0000

    using puts function. Fixing code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * main - here writing my first printing function
+ *
+ * Return: Succes(0)
+ */
 
-int main (void)
+int main(void)
 {
-  char *charname = "\"Programming is like building a multilingual puzzle";
-  puts(charname);
-  return 0;
+	char *charname = "\"Programming is like building a multilingual puzzle";
+
+	puts(charname);
+	return (0);
 }
