4fe10ea2ee5126347125e3d9902b657429069e0b
Angiecastano1634 <1634@holbertonschool.com>
2020-02-05 22:47:19 +0000

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - printf
+ *
+ * Return: Succes(0) Error(1)
+ */
+
+int main(void)
+{
+	char *star = "with proper grammar, but the outcome is a piece of art,"
+
+	printf("%s\n", star);
+	return (0);
+}
