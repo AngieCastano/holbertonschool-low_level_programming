9dd8be72b4ada6fa06b048bc7016aabf1651eb8d
AngieCastano1634 <1634@holbertonschool.com>
2020-02-05 21:52:03 -0500

    finding the size of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,10 @@
  */
 int  main(void)
 {
-	int i;
-	char c;
-	printf("Size of a char: %d", sizeof(c));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld\n", sizeof(unsigned int));
+	printf("Size of a long long int: %ld \n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
