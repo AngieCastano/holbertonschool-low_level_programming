36685418056439fab19df6706edca83afd99087f
Angiecastano1634 <1634@holbertonschool.com>
2020-02-06 21:21:45 +0000

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printf
+ *
+ * Return: Succes(0) Error(1)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i <= 97; i--)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
