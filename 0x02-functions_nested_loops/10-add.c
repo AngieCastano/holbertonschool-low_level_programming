a5e4bf78396f5bc8388e7a0b60761ec85790dc57
cristian0497 <cristian04.97@hotmail.com>
2020-02-11 14:36:53 -0500

    adding 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - tells the absolute value of a number
+ * @n: Integer as a number
+ * @f: Integer as a number
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int n, int f)
+{
+	return (n + f);
+}
