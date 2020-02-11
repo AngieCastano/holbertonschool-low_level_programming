94a054d7b92ccb6bdccc3eecf23d8cd409b888b2
Angiecastano1634 <1634@holbertonschool.com>
2020-02-11 19:32:15 +0000

    adding a new function

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,9 +1,10 @@
-#ifndef _HOLBERTON_H
+ #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
 
 #endif
