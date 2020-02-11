592a0c44b144776c8c142fd5c2a01112aa29c0c3
cristian0497 <cristian04.97@hotmail.com>
2020-02-11 16:17:16 -0500

    use the std library

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,29 +1,5 @@
 #include "holberton.h"
-/**
- * more_than - printingnatural numbers
- * @n: integer number
- * Return: 0n succes 1
- * On error
- */
-void more_than(int n)
-{
-	int i;
-
-	for (i = n; i >= 98; i--)
-	{
-		if (i > 99)
-			_putchar((i / 100) + '0');
-		_putchar((i / 10) % 10 + '0');
-		_putchar((i % 10) + '0');
-		if (i != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	_putchar('\n');
-}
-
+#include <stdio.h>
 /**
  * print_to_98 - printingnatural numbers
  * @n: integer number
@@ -32,38 +8,13 @@ void more_than(int n)
  */
 void print_to_98(int n)
 {
-	int i, x;
-
-	if (n <= 98)
+	while (n != 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i < 0)
-			{
-				x = i * -1;
-				_putchar('-');
-				if (i < -9)
-					_putchar((x / 10) + '0');
-				_putchar((x % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				if ((i / 10) != 0)
-					_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-		}
-		_putchar('\n');
+		printf("%d, ", n);
+		if (n > 98)
+			n--;
+		if (n < 98)
+			n++;
 	}
-	else
-	{
-		more_than(n);
-	}
+	printf("%d\n", n);
 }
