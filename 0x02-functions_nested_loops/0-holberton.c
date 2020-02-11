26433af14a3d8ed0a214301015854b92819b2cf4
Angiecastano1634 <1634@holbertonschool.com>
2020-02-11 15:57:54 +0000

    erasing comments

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,21 @@
-#include 'holberton.h'
+#include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
+ * main - print Holberon
  * On error, -1 is returned, and errno is set appropriately.
+ * Return: On succes 1.
  */
-_putchar("Holberton\n")
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
