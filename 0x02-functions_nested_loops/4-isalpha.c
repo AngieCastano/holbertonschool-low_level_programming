030a98f053aced12841fe91fc13a03f4be588d2e
Angiecastano1634 <1634@holbertonschool.com>
2020-02-11 19:22:49 +0000

    showing if is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isalpha - tells if a character i lower or not
+ * @c: Letter
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
