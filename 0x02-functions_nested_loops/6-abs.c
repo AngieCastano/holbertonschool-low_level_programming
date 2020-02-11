4ec20c8b882f94a06f5ce6fb19985a0a37847bcb
Angiecastano1634 <1634@holbertonschool.com>
2020-02-11 20:59:15 +0000

    printing the absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,20 @@
 #include "holberton.h"
 /**
- * print_sign - tells if a character i lower or not
- * @n: Letter
+ * _abs - tells the absolute value of a number
+ * @n: Integer as a number
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int print_sign(int n)
+int _abs(int n)
 {
 	if (n < 0)
 	{
-		n = n * -1
+		n = n * -1;
 		return (n);
 	}
 	else
-		return (n);
+	{
+		return(n);
+	}
 }
