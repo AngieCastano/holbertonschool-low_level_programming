7659a4a46408524bc1575af89aaa95f4ad239ed3
Angiecastano1634 <1634@holbertonschool.com>
2020-02-11 20:38:35 +0000

    orinting absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_sign - tells if a character i lower or not
+ * @n: Letter
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1
+		return (n);
+	}
+	else
+		return (n);
+}
