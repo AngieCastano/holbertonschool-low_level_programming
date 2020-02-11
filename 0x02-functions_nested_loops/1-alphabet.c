66bac2d9873498c899684ac6fef155a85634bcc2
Angiecastano1634 <1634@holbertonschool.com>
2020-02-11 16:15:56 +0000

    prints the alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * print_alphabet - writes the alphabet in lower case
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet(void)
+{
+	(for int i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+}
