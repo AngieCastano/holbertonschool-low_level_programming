44c09adb4ec750a9eede0be421426e960c944999
cristian0497 <cristian04.97@hotmail.com>
2020-02-11 15:22:23 -0500

    Printing 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,35 @@
  */
 void print_to_98(int n)
 {
-	int i;
+	int i, x;
 
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			if (i < 0)
+			{
+				x = i * -1;
+				_putchar('-');
+				if (i < -9)
+					_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				if ((i / 10) != 0)
+					_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+				if (i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 		}
+		_putchar('\n');
 	}
 	else
 	{
@@ -23,8 +43,14 @@ void print_to_98(int n)
 		{
 			if (i > 99)
 				_putchar((i  / 100) + '0');
-			_putchar((i / 10) + '0');
+			_putchar((i / 10) / 10 + '0');
 			_putchar((i % 10) + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
 }
