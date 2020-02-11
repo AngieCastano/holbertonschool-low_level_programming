bf0786e4b0ae5005eb76e2d932a441bc088ef07f
cristian0497 <cristian04.97@hotmail.com>
2020-02-11 15:49:38 -0500

    Printing until 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -42,8 +42,8 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			if (i > 99)
-				_putchar((i  / 100) + '0');
-			_putchar((i / 10) / 10 + '0');
+				_putchar((i / 100) + '0');
+			_putchar((i / 10) % 10 + '0');
 			_putchar((i % 10) + '0');
 			if (i != 98)
 			{
