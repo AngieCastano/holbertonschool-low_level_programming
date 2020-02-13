416ffbb979669dc993f52eb1f8a55eb34a7006c8
AngieCastano1634 <1634@holbertonschool.com>
2020-02-12 23:31:07 -0500

    Changing content

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,12 +1,6 @@
-#include "holberton.h"
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
 
-/**
- * main - print Holberon
- * On error, -1 is returned, and errno is set appropriately.
- * Return: On succes 1.
- */
-int main(void)
-{
-	int largest_number(int a, int b, int c);
-	return (0);
-}
+int largest_number(int a, int b, int c);
+
+#endif
