093365a7fa732d579efd15b14c7817ce7f1b1f89
Angiecastano1634 <1634@holbertonschool.com>
2020-02-13 19:56:10 +0000

    prints a square with size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_square - printing a square with x size
+ * @size: size
+ *
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++ )
+	{
+		for (j = 0; j < size; j++)
+			putchar('#');
+		putchar('\n');
+	}
+}
