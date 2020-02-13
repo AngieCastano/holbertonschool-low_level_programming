8a332a47d8cc494c01f4231a4f01869192f66cc3
AngieCastano1634 <1634@holbertonschool.com>
2020-02-13 01:02:26 +0000

    idea

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,6 +14,8 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			res = i * j;
+			if ((res / 100) != 0)
+				_putchar((res / 100) + '0');
 			if ((res / 10) != 0)
 				_putchar((res / 10) + '0');
 			else
@@ -29,3 +31,10 @@ void print_times_table(int n)
 		_putchar('\n');
 	}
 }
+int{}
+
+(n/10) hasta que n/10 = 0
+	for (i = 0; (n/10))// Necesitamos una funcion que nos divida el numero res hasta que su division sea igual a 0, 
+		vamos incrementando una variable para saber cuantas veces cuantos digitos tiene el numero que llegó.
+			Una vez tenemos el numero de digitos hacemos una recursion de (res / 1(y aqui el numero de ceros segun el numero de digitos de res ))
+			despues toca ir decremetando e; numero de ceros.
