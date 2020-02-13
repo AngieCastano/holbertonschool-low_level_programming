4bb09bc113cb87d8be3372c7072590982ef57c64
AngieCastano1634 <1634@holbertonschool.com>
2020-02-13 18:47:35 -0500

    fixing betty style

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,19 @@
 #include "holberton.h"
 
 /**
- * _isdigit: checks for a digit (0 through 9)
+ * _isdigit - checks for a digit (0 through 9)
  * @c: Digit to validate
  *Return: largest number
  */
 
 int _isdigit(int c)
 {
-	int d = 0;
-	if (c => '0' && c <= '9')
+	int d;
+
+	if (c >= '0' && c <= '9')
+	{
 		d = 1;
+	}
 	else
 		d = 0;
 	return (d);
