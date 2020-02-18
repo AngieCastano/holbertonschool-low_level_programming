db6b9cee84aa0cf932a396110329a5c23e7189bf
AngieCastano1634 <1634@holbertonschool.com>
2020-02-18 16:35:18 -0500

    fixing betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
-/* reset_to_98 - resets n numers to 98
-/* @n: variable to change
-/*
+/**
+* reset_to_98 - resets n numers to 98
+* @n: variable to change
+*
 */
 void reset_to_98(int *n)
 {
