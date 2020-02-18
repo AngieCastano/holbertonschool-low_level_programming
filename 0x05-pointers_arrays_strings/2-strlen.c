74e54bd1d0bb813a0147f3342053324e8a400840
AngieCastano1634 <1634@holbertonschool.com>
2020-02-18 17:02:09 -0500

    creating

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to know its length
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != \0; i++)
+	{
+	}
+	return (i);
+}
