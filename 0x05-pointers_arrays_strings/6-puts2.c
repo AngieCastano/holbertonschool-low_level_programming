1199101ff1aa04001c5fc68f9d9678e3a8ad4b34
AngieCastano1634 <1634@holbertonschool.com>
2020-02-18 19:21:28 -0500

    creating

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * puts2 - printing every other character of a function
+ * @str: string to print in odd
+ *
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j <= (i - 1); j + 2)
+	{
+		_putchar(s[j]);
+	}
+	_putcher('\n');
+}
