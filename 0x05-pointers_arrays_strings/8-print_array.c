85343573f98a6709f1fdbd20da7aef0d1244656a
AngieCastano1634 <1634@holbertonschool.com>
2020-02-18 21:43:04 -0500

    using the stdio.h library

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * print_array - prints n elements of an array of integers
  * @a: array
@@ -13,12 +14,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		_putchar(*(a + i));
+		printf("%d", *(a + i));
 		if (i != n)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf(", ");
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
