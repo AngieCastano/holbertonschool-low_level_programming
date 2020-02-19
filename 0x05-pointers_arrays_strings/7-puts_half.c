8f7fa0652ddb1866c14c043700de3c175779f541
AngieCastano1634 <1634@holbertonschool.com>
2020-02-18 19:53:29 -0500

    creating

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * puts_half - printing the second half of a string
+ * @str: string to print its second half
+ *
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	for (j = (i / 2); j <= i; i++)
+	{
+		_putchar(str[j]);
+	}
+}
