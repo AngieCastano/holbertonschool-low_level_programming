3259d12c0f114d48156dd92e3607186057608904
AngieCastano1634 <1634@holbertonschool.com>
2020-02-18 22:55:03 -0500

    creating

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * *_strcpy - copy a string
+ * @dest: The destination pointer
+ * @src: The source string
+ * Return: The pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+	i++;
+	for (j = 0; j <= i; j++)
+	{
+		dest[j] = src[j];
+	}
+	return (*dest);
+}
