964f82fad0400cd447c230ee8ffe1143575f7869
Angiecastano1634 <1634@holbertonschool.com>
2020-02-20 19:48:09 +0000

    fixin betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,16 +3,18 @@
  * _strncat - concayenating 2 strings
  * @dest: where both string become 1
  * @src: string to add to de dest
+ * @n: until when
  * Return: concatenated strings
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-	for( i = 0; dest[i] != '\0'; i++)
+
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for ( j = 0; j < n; j++)
+	for (j = 0; j < n; j++)
 		dest[i + j] = src[j];
-	return(dest);
+	return (dest);
 }
