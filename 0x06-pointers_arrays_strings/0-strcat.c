915acdd507b424d9120cc67f2bdf3fc86a2c4599
Angiecastano1634 <1634@holbertonschool.com>
2020-02-20 19:44:38 +0000

    creating

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strcat - concayenating 2 strings
+ * @dest: where both string become 1
+ * @src: string to add to de dest
+ * Return: concatenated strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	for( i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for ( j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+	return(dest);
+}
