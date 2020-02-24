9fadd0f63e75b3a398064ba0a0c5b7e579795ff7
Angiecastano1634 <1634@holbertonschool.com>
2020-02-24 19:53:23 +0000

    creating

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to match
+ * @accept: string to compare
+ * Return: he number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int long_of_s, long_of_c, i, j, match;
+
+	for (long_of_s = 0; s[long_of_s] != '\0'; long_of_s++)
+	{
+	}
+	for (long_of_c = 0; c[long_of_c] != '\0'; long_of_c++)
+	{
+	}
+	for (i = 0; i < long_of_s && i <= match; i++)
+	{
+		for (int j = 0; j < long_of_c; j++)
+		{
+			if (s[i] == c[j])
+			{
+				match = match + 1;
+				break;
+			}
+		}
+	}
+	return (match);
+}
