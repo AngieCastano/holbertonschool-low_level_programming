64e91fd7bdba0dc2ed64b4ed6c0f3387ec63d578
Angiecastano1634 <1634@holbertonschool.com>
2020-02-27 00:55:20 +0000

    creating squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find its root
+ * Return: root
+ */
+int _sqrt_recursion(int n)
+{
+	int x = n;
+
+	x = _root(x, n);
+	return (x);
+}
+/**
+ * _root - decremets a number
+ * @x: number to decrement
+ * @n: ene
+ * Return: the laste decrement
+ */
+int _root(int x, int n)
+{
+	if (x * x == n)
+		return (x);
+	else if (x == 0)
+		return (-1);
+	else if (x > 0)
+		return (_root(--x, n));
+	return (0);
+}
