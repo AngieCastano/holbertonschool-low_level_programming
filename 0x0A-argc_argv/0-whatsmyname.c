932cf0c61a85c12c60e0905233d9dcfeb5f14aa4
AngieCastano1634 <1634@holbertonschool.com>
2020-03-01 18:43:56 -0500

    creating

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- prints the progrms name
+ * @argc: quantity of argumets
+ * @argv: vector of arguments
+ * Return: 0 if succes
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
