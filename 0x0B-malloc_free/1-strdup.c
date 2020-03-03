ad02aabbd19ef32a1cfe1945c39b7b45987ed0fa
Angiecastano1634 <1634@holbertonschool.com>
2020-03-03 21:14:58 +0000

    creating

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - generates a string
+ * @str: string to recive
+ * Return: a pointer to a newly allocated space in memory
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+	}
+	copy = malloc(i * sizeof(char));
+	for (i = 0; str[i]; i++)
+	{
+		copy[i] = str[i];
+	}
+	copy[i] = '\0';
+	return (copy);
+}
