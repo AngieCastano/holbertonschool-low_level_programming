fe283a1dd151cf0d007c5f130dd225b674688148
Angiecastano1634 <1634@holbertonschool.com>
2020-03-03 20:32:09 +0000

    fixing my code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
-	str = malloc(size * sizeof(char))puntoycoma
+	str = malloc(size * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
