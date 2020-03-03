e005c1f02cbbfe87c70ac8d34be7ad459521e832
Angiecastano1634 <1634@holbertonschool.com>
2020-03-03 19:53:01 +0000

    creating

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array -  creates an initializes with specific values
+ * @size: size of array
+ * @c: char of initialization
+ * Return: the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+	str = malloc(size * sizeof(char));
+	for(i = 0; i < size; i++)
+		str[i] = c;
+	return(str);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,5 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#endif
+char *create_array(unsigned int size, char c);
