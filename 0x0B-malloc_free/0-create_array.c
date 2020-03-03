e5c16fba1334c32cd111d0ad8a94f5211e1f955d
Angiecastano1634 <1634@holbertonschool.com>
2020-03-03 20:29:34 +0000

    destroying my code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
-	str = malloc(size * sizeof(char));
+	str = malloc(size * sizeof(char))puntoycoma
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
