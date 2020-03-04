c2c4c75ddc1082a0ad9147634e97f003a4669dd1
Angiecastano1634 <1634@holbertonschool.com>
2020-03-04 21:28:18 +0000

    reasigning f to 0

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -10,23 +10,21 @@
 char *argstostr(int ac, char **av)
 {
 	int i, f = 0, k;
-	int n = 5;
 	char *s;
-	s = malloc(n * sizeof(char));
-	printf("%ld ar \n", sizeof(malloc(n * sizeof(char))));
+
 	for (i = 0; i < ac; i++)
 	{
 		for (k = 0; av[i][k] != '\0'; k++)
 		{
 			f++;
-			printf("%c\n",av[i][k]);
 		}
 		f++;
 	}
-	++f;
-	printf("%d f \n", f);
-	s = malloc(n * sizeof(char));
-	printf("%ld s \n", sizeof(s));
+	f++;
+	s = malloc(sizeof(char) * f);
+	if (s == NULL)
+		return (NULL);
+	f = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (k = 0; av[i][k] != '\0'; k++)
@@ -34,7 +32,7 @@ char *argstostr(int ac, char **av)
 			s[f] = av[i][k];
 			f++;
 		}
-                s[f++] = '\n';
+		s[f++] = '\0';
 	}
 	s[f++] = '\0';
 	return (s);
