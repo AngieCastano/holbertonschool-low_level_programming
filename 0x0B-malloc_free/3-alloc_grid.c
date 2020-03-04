fac1ec70c3b55e0a043ee6d480a32444383442d6
Angiecastano1634 <1634@holbertonschool.com>
2020-03-04 00:21:14 +0000

    addinf=g a header

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers
  * @width: width of the array
@@ -28,9 +29,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
-		{
 			grid[i][j] = 0;
-		}
 	}
 	return (grid);
 }
