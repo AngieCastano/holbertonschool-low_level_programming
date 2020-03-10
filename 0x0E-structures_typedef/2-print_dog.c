194a167ababacd5c9c64d64908f16515042ba30c
AngieCastano1634 <1634@holbertonschool.com>
2020-03-10 01:42:17 +0000

    fixing the header, and the null in the float

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,16 +10,16 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name != NULL)
-			printf("Name: %s", d->name);
+			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)");
-		if (d->age != NULL)
-			printf("Age: %.1f", d->age);
+			printf("Name: (nil)\n");
+		if (d->age != .0)
+			printf("Age: %.1f\n", d->age);
 		else
-			printf("Age: (nil)");
+			printf("Age: (nil)\n");
 		if (d->owner != NULL)
-			printf("Owner: %s", d->ower);
+			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 	}
 }
