b4d63a347ef57c9af3ff815b04677018bbc53b7b
AngieCastano1634 <1634@holbertonschool.com>
2020-03-10 03:32:49 +0000

    creating

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,7 +8,53 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog_t new = {name, age, owner};
+	dog_t *new;
+	char *cp1;
+	char *cp2;
 
+	cp1 = _strdup(name);
+	if (!cp1)
+		return (NULL);
+	cp2 = _strdup(owner);
+	if (!cp2)
+	{
+		free(cp1);
+		return (NULL);
+	}
+	new = malloc(sizeof(dog_t));
+	if (!new)
+	{
+		free(cp1);
+		free(cp2);
+		return (NULL);
+	}
+	new->name = cp1;
+	new->age = age;
+	new->owner = cp2;
 	return (new);
+}
+/**
+ * _strdup - generates a string
+ * @str: string to recive
+ * Return: a pointer to a newly allocated space in memory
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+	}
+	copy = malloc((i + 1) * sizeof(char));
+	if (copy == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+		copy[i] = str[i];
+	}
+	copy[i] = '\0';
+	return (copy);
 }
