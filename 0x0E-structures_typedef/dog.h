96556f202c7a0a7e8be6582f628cca88466b6f56
AngieCastano1634 <1634@holbertonschool.com>
2020-03-10 00:54:25 +0000

    adding ;

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-struct dog 
+/**
+ * struct dog - type struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 #endif
-dog_t *new_dog(char *name, float age, char *owner);
