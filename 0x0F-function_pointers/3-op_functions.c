3db98d85799e1e40695e0b99aba59fb074392c5a
Angiecastano1634 <1634@holbertonschool.com>
2020-03-12 01:44:51 +0000

    creating

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - prints qtity of argumets passed in the command line
+ * @a: qntity of arguments
+ * @b: not used, receives the arguments
+ * Return: 0 if succes
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - prints qtity of argumets passed in the command line
+ * @a: qntity of arguments
+ * @b: not used, receives the arguments
+ * Return: 0 if succes
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - prints qtity of argumets passed in the command line
+ * @a: qntity of arguments
+ * @b: not used, receives the arguments
+ * Return: 0 if succes
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - prints qtity of argumets passed in the command line
+ * @a: qntity of arguments
+ * @b: not used, receives the arguments
+ * Return: 0 if succes
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - prints qtity of argumets passed in the command line
+ * @a: qntity of arguments
+ * @b: not used, receives the arguments
+ * Return: 0 if succes
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
