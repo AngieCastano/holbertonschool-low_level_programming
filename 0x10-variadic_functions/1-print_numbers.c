bdebb53a2bb7cc60a457f63e7576ae84c3b2d7f1
AngieCastano1634 <1634@holbertonschool.com>
2020-03-13 17:10:07 +0000

    creating

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print_numbers
+ * @separator: separator
+ * @n: number of parameters
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int i;
+
+	va_start(a, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d", va_arg(a, int));
+		if (separator)
+			printf("%s", separator);
+	}
+	printf("%d\n", va_arg(a, int));
+}
