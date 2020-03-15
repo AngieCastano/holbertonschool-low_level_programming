877f176ec90be3cf9181bdd799f4e212710583cc
AngieCastano1634 <1634@holbertonschool.com>
2020-03-14 21:25:50 -0500

    adding NULL case

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,17 +8,22 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list a;
 	unsigned int i;
-
+	int safe;
 	va_start(a, n);
 	if (n > 0)
 	{
 		for (i = 0; i < n - 1; i++)
 		{
-			printf("%d", va_arg(a, int));
+			safe = va_arg(a, int);
+			if (safe)
+				printf("%d", safe);
 			if (separator)
 				printf("%s", separator);
 		}
-		printf("%d\n", va_arg(a, int));
+		safe = va_arg(a, int);
+		if (safe)
+			printf("%d", safe);
+		printf("\n");
 	}
 	va_end(a);
 }
