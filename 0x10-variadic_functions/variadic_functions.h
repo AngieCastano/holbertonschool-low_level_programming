cf797505f92216c4d6f4e694d8f730685495fc9d
AngieCastano1634 <1634@holbertonschool.com>
2020-03-14 21:05:57 -0500

    fixing betty

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,11 +2,16 @@
 #define _HEADER_FILE_
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ * struct ch_fmt - sees if the format matches
+ * @c: the char of the format
+ * @f: function to make
+ */
 typedef struct ch_fmt
 {
 	char c;
 	void (*f)(va_list);
-}fmt_type;
+} fmt_type;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
