78e3dc9318b06fb20fa94a96e73af1d0c42db95a
AngieCastano1634 <1634@holbertonschool.com>
2020-03-15 22:16:34 -0500

    NULL case

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n - 1; i++)
 		{
 			printf("%d", va_arg(a, int));
-			if (separator)
+			if (separator != NULL)
 				printf("%s", separator);
 		}
 			printf("%d\n", va_arg(a, int));
