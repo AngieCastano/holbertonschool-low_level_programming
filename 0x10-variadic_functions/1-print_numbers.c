e352026b4b24525ad94ba2900b3d48c9a52ec50e
AngieCastano1634 <1634@holbertonschool.com>
2020-03-15 22:06:53 -0500

    fixin null case

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,4 @@
-#include "variadic_functions.h"
+xc#include "variadic_functions.h"
 /**
  * print_numbers - print_numbers
  * @separator: separator
@@ -9,13 +9,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list a;
 	unsigned int i;
 	int safe;
+
 	va_start(a, n);
-	if (n > 0)
-	{
 		for (i = 0; i < n - 1; i++)
 		{
 			safe = va_arg(a, int);
-			if (safe)
 				printf("%d", safe);
 			if (separator)
 				printf("%s", separator);
@@ -24,6 +22,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (safe)
 			printf("%d", safe);
 		printf("\n");
-	}
 	va_end(a);
 }
