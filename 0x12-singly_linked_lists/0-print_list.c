f2bc11b3eba1dbe4e06a0bf18326db807ab3e673
AngieCastano1634 <1634@holbertonschool.com>
2020-03-19 15:09:21 -0500

    creating

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list -
+ * @h: header of the list
+ * Return: number of elements of the string
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num_of_lists = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next, num_of_lists++;
+	}
+	return (num_of_lists);
+}
