29dde3e4c5119f737d45439430aae6d91922920c
AngieCastano1634 <1634@holbertonschool.com>
2020-03-19 15:17:17 -0500

    returns quiantity of lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,17 @@
 #include "lists.h"
 
 /**
- * list_len -
- *
- * Return: 
+ * print_list -
+ * @h: header of the list
+ * Return: number of elements of the string
  */
-size_t list_len(const list_t *h)
+size_t print_list(const list_t *h)
 {
+	size_t num_of_lists = 0;
 
+	while (h)
+	{
+		h = h->next, num_of_lists++;
+	}
+	return (num_of_lists);
 }
