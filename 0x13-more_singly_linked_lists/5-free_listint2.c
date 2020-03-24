4a2ee453720249416705d3d84ff0ef34f769afe0
AngieCastano1634 <1634@holbertonschool.com>
2020-03-24 17:15:13 -0500

    Write a function that frees a listint_t list. DOUBLE POINTER

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head= head of the tail
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *copy_head, *puntero_head;
+	puntero_head = *head;
+
+	*head = NULL;
+	while (puntero_head)
+	{
+		copy_head = puntero_head->next;
+		free(puntero_head);
+		puntero_head = copy_head;
+	}
+}
