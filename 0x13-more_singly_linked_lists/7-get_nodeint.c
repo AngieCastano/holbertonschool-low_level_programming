7f80acf9ee932b9ea789e97474e635ee010393e1
AngieCastano1634 <1634@holbertonschool.com>
2020-03-24 21:44:29 -0500

    adding 0 edge case

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,6 +12,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (!head)
 		return (NULL);
+	if (index == 0)
+		return (head);
 	while (head && head->next && i < index - 1)
 	{
 		head = head->next;
