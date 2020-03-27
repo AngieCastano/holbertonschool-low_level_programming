9fb00fb8dacc0fd765aafce9b1910a15cfc02456
AngieCastano1634 <1634@holbertonschool.com>
2020-03-26 19:56:19 -0500

    creating again

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,56 +1,12 @@
 #include "holberton.h"
 /**
- * return_binary - print_binary
- * @n: to  covert to binary
- * Return: char
- */
-char *return_binary(unsigned long int n)
-{
-	char *a;
-	int i = 0, base = 2;
-	unsigned long int f = n;
-
-	if (f == 0)
-	{
-		a = "0";
-		return (a);
-	}
-	while (f > 0)
-	{
-		f /= base;
-		++i;
-	}
-	a = malloc(sizeof(char) * (i + 1));
-	if (!a)
-	{
-		free(a);
-		return (NULL);
-	}
-	i = 0, f = n;
-	while (f > 0)
-	{
-		a[i] = f % base + 48;
-		f /= base;
-		++i;
-	}
-	a[i] = '\0';
-	return (a);
-}
-/**
  * get_bit - returns the value of a bit at a given index.
- * @n: number to evaluate
+ * @n: n to look at
  * @index: index
- * Return: the value of the bit at index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	char *binary;
-
-	binary = return_binary(n);
 	if (index >= (sizeof(unsigned long int) * 8) - 1)
 		return (-1);
-	if (!binary[index])
-		return (-1);
-	n = binary[index] - 48;
-	return (n);
+	return((n >> index) & 1);
 }
