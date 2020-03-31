6fc425eda040644c444a6616ec7ba2650612af56
AngieCastano1634 <1634@holbertonschool.com>
2020-03-31 16:52:20 -0500

    o_rwrd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,14 +13,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	for (i = 0; text_content[i]; i++)
 	{
 	}
 	if (!text_content)
+	{
+		close(fd);
 		return (1);
+	}
 	write1 = write(fd, text_content, i);
 	if (write1 == -1)
 	{
