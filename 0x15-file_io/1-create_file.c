18ea4ed12637ed571f126c34d62ab8577bddef2f
AngieCastano1634 <1634@holbertonschool.com>
2020-03-31 16:56:02 -0500

    cambia el for de lugar

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,13 +16,13 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	for (i = 0; text_content[i]; i++)
-	{
-	}
 	if (!text_content)
 	{
 		close(fd);
 		return (1);
+	}
+	for (i = 0; text_content[i]; i++)
+	{
 	}
 	write1 = write(fd, text_content, i);
 	if (write1 == -1)
