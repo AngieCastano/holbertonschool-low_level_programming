b38c873383d57ca566e96a3309919cf5132dbe07
AngieCastano1634 <1634@holbertonschool.com>
2020-03-31 15:15:02 -0500

    fixing betty and adding an edge case

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,15 +2,15 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX
- * @filename - filename
- * @letters - num_of_letters
+ * @filename: filename
+ * @letters: num_of_letters
  * Return: how many letters were printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	int fd;
-	ssize_t read1;
+	ssize_t read1, write1;
 
 	buf = malloc(letters);
 	if (!filename)
@@ -19,7 +19,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	read1 = read(fd, buf, letters);
-	write(1, buf, read1);
+	write1 = write(1, buf, read1);
+	if (write1 == 0 || write1 == -1)
+		return (0);
 	close(fd);
 	return (read1);
 }
