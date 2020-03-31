42b44dccd157d4f279bde88ba72fadeb3a4aa449
AngieCastano1634 <1634@holbertonschool.com>
2020-03-31 15:34:37 -0500

    adding edge cases

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buf = malloc(letters);
 	if (!buf)
+	{	free(buf);
 		return (0);
+	}
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY, 0777);
