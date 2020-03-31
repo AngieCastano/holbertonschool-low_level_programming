56ac9501e2a555d8277842abf89dd7e2a3e2ca75
AngieCastano1634 <1634@holbertonschool.com>
2020-03-31 16:24:52 -0500

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,12 @@ int create_file(const char *filename, char *text_content)
 	{
 	}
 	if (!text_content)
-	        return (1);
+		return (1);
 	write1 = write(fd, text_content, i);
 	if (write1 == -1 || write1 != i)
 	{
 		return (-1);
 	}
+	close(fd);
 	return (1);
 }
