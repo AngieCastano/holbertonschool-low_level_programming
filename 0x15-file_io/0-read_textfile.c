8d5773db2e50834e55dd804f174049043c542454
AngieCastano1634 <1634@holbertonschool.com>
2020-03-31 19:02:50 -0500

    does not validate the write

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,24 +10,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	int fd;
-	ssize_t read1, write1;
+	ssize_t read1;
 
 	buf = malloc(letters);
-	if (!buf)
+	if (!buf || !filename)
 	{	free(buf);
 		return (0);
 	}
-	if (!filename)
-		return (0);
-	fd = open(filename, O_RDONLY, 0777);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	read1 = read(fd, buf, letters);
 	if (read1 == -1)
 		return (0);
-	write1 = write(1, buf, read1);
-	if (write1 != read1 || write1 == -1)
-		return (0);
+	write(STDOUT_FILENO, buf, read1);
+	free(fd);
 	close(fd);
 	return (read1);
 }
