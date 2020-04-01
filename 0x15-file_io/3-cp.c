cce39af50a7a2a06180ce319c55deaca76b979aa
AngieCastano1634 <1634@holbertonschool.com>
2020-03-31 22:18:17 -0500

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,39 +8,30 @@
  */
 int main(int ac, char **av)
 {
-	int fd1, fd2, close1, close2, read1, dprintf1;
+	int fd1, fd2, close1, close2, read1, write1;
 	char buf[1024];
 
 	if (ac != 3)
 		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
-	fd2 = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	if (fd2 == -1)
-		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	fd1 = open(av[1], O_RDONLY);
-	if (fd1 == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 	read1 = read(fd1, buf, 1024);
-	if (read1 == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
-	while (read1 == 1024)
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while (read1 > 0)
 	{
-		read1 = read(fd1, buf, 1024);
-		if (read1 == -1)
+		if (read1 == -1 || fd1 == -1)
 		{
 			dprintf(2, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
-		dprintf1 = write(fd2, buf, read1);
-		if (dprintf1 == -1)
+		write1 = write(fd2, buf, read1);
+		if (write1 == -1 || fd2 == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
+		read1 = read(fd1, buf, 1024);
+		fd2 = open(av[2], O_WRONLY | O_APPEND);
 	}
-	buf[1024] = '\n';
-	dprintf1 = write(fd2, buf, read1);
-	if (dprintf1 == -1)
-		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	close1 = close(fd1), close2 = close(fd2);
 	if (close1 == -1)
 		dprintf(2, "Error: Can't close fd %i\n", close1), exit(100);
