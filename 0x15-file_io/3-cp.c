a5e8a7832b4f54c4000a797bf2d0c9837f796caa
AngieCastano1634 <1634@holbertonschool.com>
2020-03-31 22:21:37 -0500

    changing while for do while

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,8 +16,7 @@ int main(int ac, char **av)
 	fd1 = open(av[1], O_RDONLY);
 	read1 = read(fd1, buf, 1024);
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while (read1 > 0)
-	{
+	do {
 		if (read1 == -1 || fd1 == -1)
 		{
 			dprintf(2, "Error: Can't read from file %s\n", av[1]);
@@ -31,7 +30,7 @@ int main(int ac, char **av)
 		}
 		read1 = read(fd1, buf, 1024);
 		fd2 = open(av[2], O_WRONLY | O_APPEND);
-	}
+	} while (read1 > 0);
 	close1 = close(fd1), close2 = close(fd2);
 	if (close1 == -1)
 		dprintf(2, "Error: Can't close fd %i\n", close1), exit(100);
