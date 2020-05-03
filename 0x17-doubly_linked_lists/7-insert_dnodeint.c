4794bc5044141cbaa3d6ec6aecb68669d416598f
AngieCastano1634 <1634@holbertonschool.com>
2020-05-03 15:25:53 -0500

    fixing adding node at the end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,25 +24,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	while (copy && copy->next && index != idx)
 	{
-		copy = copy->next;
-		index++;
+		copy = copy->next, index++;
 	}
 	if (idx == 0)
 	{
 		new_node->next = *h;
 		new_node->prev = NULL;
-		(*h)->prev = new_node;
-		*h = new_node;
+		(*h)->prev = new_node, *h = new_node;
 		return (new_node);
 	}
 	if (index == idx)
 	{
-		copy->prev->next = new_node;
-		new_node->next = copy;
+		copy->prev->next = new_node, new_node->next = copy;
 		new_node->prev = copy->prev;
 		copy->prev = new_node;
 		return (new_node);
 	}
+	else if (!copy->next)
+	{
+		new_node->prev = copy, new_node->next = NULL;
+		copy->next = new_node;
+		return (new_node);
+	}
 	else
 		return (NULL);
+
 }
