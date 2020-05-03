27e3c515f78a1b2ecdc6dcf655ef3f6cb1d6dba7
AngieCastano1634 <1634@holbertonschool.com>
2020-05-03 18:34:17 -0500

    adds node at the last+1 index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,11 +10,55 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int index = 0;
-	dlistint_t *new_node = NULL, *copy = NULL;
+	dlistint_t *new_node = NULL, *copy = NULL, *copy_copy = NULL;
 
 	if (!h || (!*h && idx != 0))
 		return (NULL);
 	copy = *h;
+	while (copy && index != idx)
+		copy_copy = copy, copy = copy->next, index++;
+	if (idx == 0)
+	{
+		if (!*h)
+		{
+			new_node = create_node(n);
+			new_node->next = NULL, new_node->prev = NULL;
+			*h = new_node;
+			return (new_node);
+		}
+		new_node = create_node(n);
+		new_node->next = *h, new_node->prev = NULL;
+		(*h)->prev = new_node, *h = new_node;
+		return (new_node);
+	}
+	if (index == idx && copy)
+	{
+		new_node = create_node(n);
+		copy->prev->next = new_node, new_node->next = copy;
+		new_node->prev = copy->prev, copy->prev = new_node;
+		return (new_node);
+	}
+	if (index == idx && !copy)
+	{
+		new_node = create_node(n);
+		copy_copy->next = new_node, new_node->next = NULL;
+		new_node->prev = copy_copy;
+		return (new_node);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+/**
+ * create_node - creates a new node
+ * @n: data to add
+ * Return: new node
+**/
+dlistint_t *create_node(int n)
+{
+	dlistint_t *new_node = NULL;
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 	{
@@ -22,31 +66,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	while (copy && copy->next && index != idx)
-	{
-		copy = copy->next, index++;
-	}
-	if (idx == 0)
-	{
-		if (!*h)
-		{
-			new_node->next = NULL, new_node->prev = NULL;
-			*h = new_node;
-			return (new_node);
-		}
-		new_node->next = *h, new_node->prev = NULL;
-		(*h)->prev = new_node, *h = new_node;
-		return (new_node);
-	}
-	if (index == idx)
-	{
-		copy->prev->next = new_node, new_node->next = copy;
-		new_node->prev = copy->prev, copy->prev = new_node;
-		return (new_node);
-	}
-	else
-	{
-		free(new_node);
-		return (NULL);
-	}
+	return (new_node);
 }
