813f38592ca273a76b73b3fe4e5bc549c8091b77
AngieCastano1634 <1634@holbertonschool.com>
2020-05-03 17:45:40 -0500

    when head is NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int index = 0;
 	dlistint_t *new_node = NULL, *copy = NULL;
 
+	if (!h || (!*h && idx != 0))
+		return (NULL);
 	copy = *h;
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
@@ -28,8 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (!*h)
 		{
-			new_node->next = NULL;
-			new_node->prev = NULL;
+			new_node->next = NULL, new_node->prev = NULL;
 			*h = new_node;
 			return (new_node);
 		}
