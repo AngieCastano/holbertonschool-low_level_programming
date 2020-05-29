903914ce5cc1790313442fc9db5cde1926cdb61d
AngieCastano1634 <1634@holbertonschool.com>
2020-05-29 14:08:21 -0500

    [function that adds an element to the hash table.][adding node at the beginnig}

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,41 +17,35 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (*key == '\0' || !key)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	new = add_nodeint_end(&ht->array[index], key, value);
+	new = add_nodeint(&ht->array[index], key, value);
 	if (new)
 		return (1);
 	return (0);
 }
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list.
- * @head: double pointer to the head of the list
+ * add_nodeint - adds a new node at the beginning of a list
+ * @head: head of the list
  * @ke: key to add
  * @v: value to add
  * Return: the address of the new element, or NULL if it failed
  */
-hash_node_t *add_nodeint_end(hash_node_t **head, const char *ke, const char *v)
+hash_node_t *add_nodeint(hash_node_t **head, const char *ke, const char *v)
 {
-	hash_node_t *new_end_list;
-	hash_node_t *temp = *head;
+	hash_node_t *new;
 
-	new_end_list = malloc(sizeof(hash_node_t));
-	if (!new_end_list)
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
 		return (NULL);
-	new_end_list->key = strdup(ke);
-	new_end_list->value = strdup(v);
-	new_end_list->next = NULL;
-	if (!*head)
+	new->key = strdup(ke);
+	new->value = strdup(v);
+	new->next = NULL;
+	if (head || *head)
 	{
-		*head = new_end_list;
+		new->next = *head;
+		*head = new;
 	}
 	else
-	{
-		while (temp && temp->next)
-		{
-			temp = temp->next;
-		}
-		temp->next = new_end_list;
-	}
-	return (new_end_list);
+		return (NULL);
+	return (new);
 }
