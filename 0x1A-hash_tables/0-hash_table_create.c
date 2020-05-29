5aea243bcc5dc82fe18649216e52ffc1c85eb262
AngieCastano1634 <1634@holbertonschool.com>
2020-05-29 10:37:06 -0500

    [Create a hash table][validating mallocs]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 
 	ht = malloc(sizeof(hash_table_t *));
+	if (!ht)
+	{
+		return (NULL);
+	}
 	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
 	return (ht);
 }
