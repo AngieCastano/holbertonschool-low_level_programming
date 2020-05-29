94d0874b6fc57c59612031891fdbef6354bc1a82
AngieCastano1634 <1634@holbertonschool.com>
2020-05-29 10:56:06 -0500

    [Create a hash table][using calloc in a reverse way]

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
-	ht->array = calloc(sizeof(hash_node_t *), size);
+	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (!ht->array)
 	{
 		free(ht);
