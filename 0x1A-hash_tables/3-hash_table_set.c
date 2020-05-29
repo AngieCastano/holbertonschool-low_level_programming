14117def426f6ec52023dab742784121a201b4ca
AngieCastano1634 <1634@holbertonschool.com>
2020-05-29 16:06:29 -0500

    [function that adds an element to the hash table.][check for update key value}

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new;
+	hash_node_t *new, *copy;
 
 	if (!value)
 	{
@@ -19,6 +19,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht && key && *key != '\0')
 	{
 		index = key_index((unsigned char *)key, ht->size);
+		copy = ht->array[index];
+		if (update_key(copy, key,value) == 1)
+			return (1);
 		new = add_nodeint(&ht->array[index], key, value);
 		if (new)
 			return (1);
@@ -51,4 +54,23 @@ hash_node_t *add_nodeint(hash_node_t **head, const char *ke, const char *v)
 	else
 		return (NULL);
 	return (new);
+}
+/**
+ * update_key - looks if a key already exists
+ * @head: list head t traverse
+ * @compare: string to compare
+ * Return: 1 if it already exists, 0 if it doesn't
+ */
+int update_key(hash_node_t *head, const char *compare, const char *value)
+{
+	while(head)
+	{
+		if (strcmp(compare, head->key) == 0)
+		{
+			head->value = (char *) value;
+			return (1);
+		}
+		head = head->next;
+	}
+	return (0);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -38,6 +38,7 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_node_t *add_nodeint(hash_node_t **head, const char *k, const char *v);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+int update_key(hash_node_t *head, const char *compare, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
