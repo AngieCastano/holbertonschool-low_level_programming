ca88635f7d84c555058ecc49a8d2d7e8aacf549c
AngieCastano1634 <1634@holbertonschool.com>
2020-08-26 16:21:06 -0500

    [ADD][linear algorithm]

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+/**
+ * linear_search - linear search algorithm
+ * @array: array where to find
+ * @size: array's size
+ * @value: value to search
+ * Return: return the first index where value is located, else -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	if (!array || !size || !*array)
+		return (-1);
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+	}
+	return (-1);
+}
